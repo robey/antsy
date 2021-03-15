27ddceeff2ae43848825058789d72f424003e2d1
Robey Pointer <robeypointer@gmail.com>
2021-03-14 17:54:59 -0700

    export rgb_to_xterm

--- src/antsy.ts
+++ src/antsy.ts
@@ -3,4 +3,4 @@ export { Constraint, GridLayout } from "./antsy/grid_layout";
 export { Key, KeyParser, KeyType, Modifier } from "./antsy/keys";
 export { Terminal } from "./antsy/terminal";
 export { TextBuffer } from "./antsy/text_buffer";
-export { get_color, name_to_rgb, xterm_to_rgb } from "./antsy/xterm256";
+export { get_color, name_to_rgb, rgb_to_xterm, xterm_to_rgb } from "./antsy/xterm256";
